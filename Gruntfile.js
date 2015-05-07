4ee90ffa936f632111008aff44b3a3d4e1d372fc
patrickm53 <patrickmcenaney53@gmail.com>
2015-05-07 15:58:29 +0200

    Grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,21 @@
  *
  * grunt update
  *    Updates the local common files with the ones from the editor project
+
+ * Create a new bugfix version (x.y.++):
+ *   grunt v:patch
+ *   grunt minify
+ *   grunt bump-commit
+ *
+ * Create a new feature version (x.++.0)
+ *   grunt v:minor
+ *   grunt minify
+ *   grunt bump-commit
+ *
+ * Create a new major version (++.0.0)
+ *   grunt v:major
+ *   grunt minify
+ *   grunt bump-commit
  *
  * Created by kc on 14.04.15.
  */
@@ -62,5 +77,19 @@ module.exports = function (grunt) {
 
   });
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-bump');
+  grunt.registerTask('default', ['uglify:js']);
+  grunt.registerTask('minify', ['uglify:js']);
+  grunt.registerTask('v:patch', ['bump-only:patch']);
+  grunt.registerTask('v:minor', ['bump-only:minor']);
+  grunt.registerTask('v:major', ['bump-only:major']);
+  grunt.registerTask('demo', ['shell']);
   grunt.registerTask('update', ['copy']);
 };
