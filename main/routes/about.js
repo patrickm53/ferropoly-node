be88398041edc7bca6148180193bbd6bf3a56b28
patrickm53 <patrickmcenaney53@gmail.com>
2015-06-04 19:54:00 +0200

    about page

--- main/routes/about.js
+++ main/routes/about.js
@@ -6,11 +6,11 @@
 
 var express = require('express');
 var router = express.Router();
-
+var settings = require('../settings');
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('about', {title: 'Ferropoly Info'});
+  res.render('about', {title: 'Ferropoly Info', versionInfo: settings.version});
 });
 
 module.exports = router;
