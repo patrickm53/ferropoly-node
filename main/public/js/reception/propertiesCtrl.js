b0d8de7484e5f50015fcbef353e2087e77acb427
patrickm53 <patrickmcenaney53@gmail.com>
2015-06-19 06:03:47 +0200

    Basic implementation for pricelist

--- main/public/js/reception/propertiesCtrl.js
+++ main/public/js/reception/propertiesCtrl.js
@@ -8,7 +8,23 @@
  * The angular controller for the properties statistics
  */
 ferropolyApp.controller('propertiesCtrl', propertiesCtrl);
-function propertiesCtrl($scope, $http) { }
+function propertiesCtrl($scope, $http) {
+  $scope.properties = [];
+
+  $scope.updateProperties = function() {
+    dataStore.updateProperties(undefined, function() {
+      $scope.properties = dataStore.getProperties();
+      console.log('propertiesCtrl length: ' + $scope.properties.length);
+      $scope.$apply();
+    });
+  };
+
+  $scope.getTeamName = function(teamId) {
+    return dataStore.teamIdToTeamName(teamId);
+  }
+
+  registerPanelUpdateHandler('#panel-properties', $scope.updateProperties);
+}
 
 
 propertiesCtrl.$inject = ['$scope', '$http'];
