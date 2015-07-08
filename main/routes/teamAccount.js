c96c8eb120b2208df571b3d1ed0ca80b364a4158
patrickm53 <patrickmcenaney53@gmail.com>
2015-07-08 17:03:15 +0200

    balance in team account view added

--- main/routes/teamAccount.js
+++ main/routes/teamAccount.js
@@ -8,6 +8,7 @@
 var express = require('express');
 var router = express.Router();
 var teamAccount = require('../lib/accounting/teamAccount');
+var _ = require('lodash');
 
 router.get('/get/:gameId/:teamId', function (req, res) {
   if (!req.params.gameId) {
@@ -17,9 +18,19 @@ router.get('/get/:gameId/:teamId', function (req, res) {
     req.params.teamId = undefined;
   }
 
+  var teamBalance = {};
+
   teamAccount.getAccountStatement(req.params.gameId, req.params.teamId, function(err, data) {
     if (err) {
       return res.send({status: 'error', message: err.message});
+    }
+
+    for (var i = 0; i < data.length; i++) {
+      if (_.isUndefined(teamBalance[data[i].teamId])) {
+        teamBalance[data[i].teamId] = 0;
+      }
+      teamBalance[data[i].teamId] += data[i].transaction.amount;
+      data[i].balance = teamBalance[data[i].teamId];
     }
     res.send({status:'ok', accountData: data});
   });
