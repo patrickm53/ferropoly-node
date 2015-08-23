52084dc8da616fa3ea7ef2cc60b038a40c519a2d
patrickm53 <patrickmcenaney53@gmail.com>
2015-08-23 20:37:29 +0200

    adding settings...

--- main/settings/contabo_preview.js
+++ main/settings/contabo_preview.js
@@ -1,5 +1,40 @@
 /**
- *
- * Created by kc on 23.08.15.
+ * Settings for the contabo PREVIEW server, its IP is 5.189.159.156
+ * Created by kc on 15.08.15.
  */
 'use strict';
+
+module.exports = function (settings) {
+
+  settings.server = {
+    port: process.env.FERROPOLY_MAIN_PORT,
+    host: 'app.ferropoly.ch',
+    serverId: 'app.ferropoly.ch-v' + settings.version
+  };
+
+  settings.locationDbSettings = {
+    mongoDbUrl: process.env.FERROPOLY_CONNECTION_STRING
+  };
+
+  settings.cron = {
+    // [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK] [YEAR (optional)]
+  };
+
+
+  settings.socketIoServer = {
+    port: 80, // using proxy!
+    host: 'spiel-preview.ferropoly.ch'
+  };
+
+  settings.scheduler = {
+    // The preview instance shall do only something if there is no other going to handle it
+    delay: 0
+  };
+
+  settings.autopilot = {
+    interval: 263445,
+    active: true
+  };
+
+  return settings;
+};
