5a9f2d9fd18c25d91b505124edae528efd52f322
patrickm53 <patrickmcenaney53@gmail.com>
2015-08-31 17:37:39 +0200

    Loading first time ranking list is not the same as "no ranking list"

--- main/public/js/reception/dashboardCtrl.js
+++ main/public/js/reception/dashboardCtrl.js
@@ -8,6 +8,7 @@ ferropolyApp.controller('dashboardCtrl', dashboardCtrl);
 function dashboardCtrl($scope, $http) {
   $scope.rankingList = [];
   $scope.teamIdToTeamName = dataStore.teamIdToTeamName;
+  $scope.rankingListLoaded = false;
 
   $scope.refreshRankingList = function() {
     dataStore.getRankingList(function(err, list) {
@@ -18,6 +19,7 @@ function dashboardCtrl($scope, $http) {
       else {
         $scope.rankingList = list;
       }
+      $scope.rankingListLoaded = true;
       $scope.$apply();
       console.log('Ranking list received');
       console.log($scope.rankingList);
