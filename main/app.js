534e47ea127c8112943775a4d53cf4fe304ba7e3
patrickm53 <patrickmcenaney53@gmail.com>
2015-12-26 20:38:56 +0100

    Added Facebook Login, #30

--- main/app.js
+++ main/app.js
@@ -6,33 +6,32 @@
  */
 'use strict';
 var express = require('express');
-var path = require('path');
-var morgan = require('morgan');
-//var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var settings = require('./settings');
-var authStrategy = require('../common/lib/authStrategy');
-var passport = require('passport');
-var flash = require('connect-flash');
-var session = require('express-session');
+var path    = require('path');
+var morgan  = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser  = require('body-parser');
+var settings    = require('./settings');
+var passport    = require('passport');
+var flash       = require('connect-flash');
+var session     = require('express-session');
 var compression = require('compression');
-var MongoStore = require('connect-mongo')(session);
-var moment = require('moment');
+var MongoStore  = require('connect-mongo')(session);
+var moment      = require('moment');
 // Model includes
 var users = require('../common/models/userModel');
 //var gameplays = require('../common/models/gameplayModel');
-var properties = require('../common/models/propertyModel');
-var locations = require('../common/models/locationModel');
+var properties  = require('../common/models/propertyModel');
+var locations   = require('../common/models/locationModel');
 var ferropolyDb = require('../common/lib/ferropolyDb');
 // Routes includes
-var login = require('./routes/login');
-var authtoken = require('./routes/authtoken');
-var app = express();
-var ferroSocket = require('./lib/ferroSocket');
-var autopilot = require('./lib/autopilot');
-var logger = require('../common/lib/logger').getLogger('main:app');
-var logs = require('../common/models/logModel');
-
+var login        = require('./routes/login');
+var authtoken    = require('./routes/authtoken');
+var app          = express();
+var ferroSocket  = require('./lib/ferroSocket');
+var autopilot    = require('./lib/autopilot');
+var logger       = require('../common/lib/logger').getLogger('main:app');
+var logs         = require('../common/models/logModel');
+var authStrategy = require('../common/lib/authStrategy')(settings, users);
 logger.info('Ferropoly Copyright (C) 2015 Christian Kuster, CH-8342 Wernetshausen');
 logger.info('This program comes with ABSOLUTELY NO WARRANTY;');
 logger.info('This is free software, and you are welcome to redistribute it');
@@ -52,8 +51,6 @@ ferropolyDb.init(settings, function (err) {
 
   var server = require('http').Server(app);
 
-  authStrategy.init(settings, users);
-
   // view engine setup
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
@@ -66,7 +63,7 @@ ferropolyDb.init(settings, function (err) {
   app.use(morgan(':prefix :method :status :remote-addr :url'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: false}));
-  // app.use(cookieParser());
+  app.use(cookieParser());
 
   // Using compression speeds up the connection (and uses much less data for mobile)
   app.use(compression());
@@ -75,23 +72,27 @@ ferropolyDb.init(settings, function (err) {
   app.use('/info', require('./routes/info'));
 
   // Define Strategy, login
-  passport.use(authStrategy.strategy);
+  passport.use(authStrategy.facebookStrategy);
+  passport.use(authStrategy.localStrategy);
   // Session serializing of the user
   passport.serializeUser(authStrategy.serializeUser);
   // Session deserialisation of the user
   passport.deserializeUser(authStrategy.deserializeUser);
   // required for passport: configuration
   app.use(session({
-    secret: 'ferropolyIsAGameWithAVeryLargePlayground!',
-    resave: false,
+    secret           : 'ferropolyIsAGameWithAVeryLargePlayground!',
+    resave           : false,
     saveUninitialized: true,
-    cookie: {secure: false}, // This is important! secure works only for https, with http no cookie is set!!!!
-    store: new MongoStore({mongooseConnection: ferropolyDb.getDb()})
+    cookie           : {secure: false}, // This is important! secure works only for https, with http no cookie is set!!!!
+    store            : new MongoStore({mongooseConnection: ferropolyDb.getDb()})
   })); // session secret
   app.use(passport.initialize());
   app.use(passport.session()); // persistent login sessions
   app.use(flash()); // use connect-flash for flash messages stored in session
   app.use(express.static(path.join(__dirname, 'public')));
+
+  // Set auth route
+  require('../common/routes/auth')(app);
 
   // Routes initialisation
   app.use('/appinfo', require('../common/routes/info'));
@@ -118,7 +119,7 @@ ferropolyDb.init(settings, function (err) {
 
   // Now it is time to start the scheduler (after initializing ferroSocket, is required by marketplace)
   var gameScheduler = require('./lib/gameScheduler');
-  var marketplace = require('./lib/accounting/marketplace').createMarketplace(gameScheduler);
+  var marketplace   = require('./lib/accounting/marketplace').createMarketplace(gameScheduler);
   gameScheduler.update(function (err) {
     if (err) {
       logger.info('Error while updating scheduler');
@@ -127,7 +128,7 @@ ferropolyDb.init(settings, function (err) {
 
   // catch 404 and forward to error handler
   app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    var err    = new Error('Not Found');
     err.status = 404;
     next(err);
   });
@@ -139,7 +140,7 @@ ferropolyDb.init(settings, function (err) {
       res.status(err.status || 500);
       res.render('error', {
         message: err.message,
-        error: err
+        error  : err
       });
     });
   }
@@ -150,7 +151,7 @@ ferropolyDb.init(settings, function (err) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: {}
+      error  : {}
     });
   });
 
