dd45c9ca2bccabdbb1b73a0f136954dba2e9a331
patrickm53 <patrickmcenaney53@gmail.com>
2016-01-04 06:32:37 +0100

    First view of gameplays form a team perspective

--- main/public/js/indexmctrl.js
+++ main/public/js/indexmctrl.js
@@ -8,7 +8,7 @@ moment.locale('de');
 var indexControl = angular.module('indexApp', []);
 indexControl.controller('indexCtrl', ['$scope', '$http', function ($scope, $http) {
 
-  $scope.user = user;
+  $scope.user      = user;
   $scope.gameplays = [];
   var authToken;
   $scope.gameplayToDelete;
@@ -17,18 +17,16 @@ indexControl.controller('indexCtrl', ['$scope', '$http', function ($scope, $http
    * Get the auttoken (async!)
    */
   var getAuthToken = function () {
-    $http.get('/authtoken').
-      success(function (data) {
-        authToken = data.authToken;
-        console.log('Auth ok');
-      }).
-      error(function (data, status) {
-        console.log('error:');
-        console.log(data);
-        console.log(status);
-        $scope.panel = 'error';
-        $scope.errorMessage = 'Authentisierungsfehler. Status: ' + status;
-      });
+    $http.get('/authtoken').success(function (data) {
+      authToken = data.authToken;
+      console.log('Auth ok');
+    }).error(function (data, status) {
+      console.log('error:');
+      console.log(data);
+      console.log(status);
+      $scope.panel        = 'error';
+      $scope.errorMessage = 'Authentisierungsfehler. Status: ' + status;
+    });
   };
 
   $scope.isGameRunning = function (gp) {
@@ -65,10 +63,10 @@ indexControl.controller('indexCtrl', ['$scope', '$http', function ($scope, $http
   /**
    * Creates organisator information
    */
-  $scope.createOrganisatorInfo = function(gp) {
+  $scope.createOrganisatorInfo = function (gp) {
     var retVal = '';
 
-    if(gp.owner && gp.owner.organisatorName) {
+    if (gp.owner && gp.owner.organisatorName) {
       retVal += gp.owner.organisatorName + ' ';
     }
     retVal += gp.internal.owner;
@@ -76,30 +74,25 @@ indexControl.controller('indexCtrl', ['$scope', '$http', function ($scope, $http
   };
   // When document ready, load gameplays
   $(document).ready(function () {
-    $http.get('/gameplays').
-      success(function (data) {
-        if (data.success) {
-          $scope.gameplays = data.gameplays;
-        }
-        else {
-          $scope.gameplays = [];
-        }
-        console.log(data);
-        console.log('Gameplays loaded, nb:' + $scope.gameplays.length);
+    console.log('READY');
+    $http.get('/gameplays').success(function (data) {
+      $scope.gameplays = data.gameplays;
+      $scope.games = data.games;
+      console.log(data);
+      console.log('Gameplays loaded, nb:' + $scope.gameplays.length);
 
-        $scope.gameplays.forEach(function (gp) {
-          var d = new Date(gp.log.lastEdited);
-          console.log(d);
-          console.log(gp.log.lastEdited);
-        });
-        getAuthToken();
-      }).
-      error(function (data, status) {
-        console.log('error:');
-        console.log(data);
-        console.log(status);
-        $scope.gameplays = [];
+      $scope.gameplays.forEach(function (gp) {
+        var d = new Date(gp.log.lastEdited);
+        console.log(d);
+        console.log(gp.log.lastEdited);
       });
+      getAuthToken();
+    }).error(function (data, status) {
+      console.log('error:');
+      console.log(data);
+      console.log(status);
+      $scope.gameplays = [];
+    });
   });
 
 
--- main/routes/index.js
+++ main/routes/index.js
@@ -5,13 +5,15 @@
 'use strict';
 
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
-var settings = require('../settings');
+var settings      = require('../settings');
 var gameplayModel = require('../../common/models/gameplayModel');
-var users = require('../../common/models/userModel');
-var logger = require('../../common/lib/logger').getLogger('routes:index');
-var errorHandler = require('../lib/errorHandler');
+var users         = require('../../common/models/userModel');
+var logger        = require('../../common/lib/logger').getLogger('routes:index');
+var errorHandler  = require('../lib/errorHandler');
+var teams         = require('../../common/models/teamModel');
+var async         = require('async');
 
 var ngFile = '/js/indexctrl.js';
 if (settings.minifedjs) {
@@ -25,35 +27,75 @@ router.get('/', function (req, res) {
       return errorHandler(res, 'Interner Fehler beim Laden des Users.', err, 500);
     }
     res.render('index', {
-      title: 'Ferropoly Spielauswertung',
+      title       : 'Ferropoly Spielauswertung',
       ngController: 'indexCtrl',
-      ngApp: 'indexApp',
-      ngFile: ngFile,
-      user: user,
-      userJson: JSON.stringify(user)
+      ngApp       : 'indexApp',
+      ngFile      : ngFile,
+      user        : user,
+      userJson    : JSON.stringify(user)
     });
 
   });
 });
 
+/**
+ * Get the gameplays for the user, the ones owned and the ones as player
+ */
 router.get('/gameplays', function (req, res) {
   gameplayModel.getGameplaysForUser(req.session.passport.user, function (err, gameplays) {
     if (err) {
       logger.error('can not get gameplays for a user', err);
-      return res.send({success: false, message: err.message});
+      return res.status(500).send({message: err.message});
     }
-    var retVal = {success: true, gameplays: []};
+    var retVal = {success: true, gameplays: [], games: []};
     if (gameplays) {
       gameplays.forEach(function (gameplay) {
         retVal.gameplays.push({
-          internal: gameplay.internal,
-          gamename: gameplay.gamename,
+          internal  : gameplay.internal,
+          gamename  : gameplay.gamename,
           scheduling: gameplay.scheduling,
-          log: gameplay.log
+          log       : gameplay.log
         });
       });
     }
-    return res.send(retVal);
+
+    teams.getMyTeams(req.session.passport.user, function (err, myTeams) {
+      if (err) {
+        logger.error('can not get teams for a user', err);
+        return res.status(500).send({message: err.message});
+      }
+      if (!myTeams) {
+        // No teams as player. return
+        return res.send(retVal);
+      }
+
+      async.each(myTeams,
+        function (team, cb) {
+          // Add all gameplays for each team found
+          gameplayModel.getGameplay(team.gameId, null, function (err, gp) {
+            if (err) {
+              return cb(err);
+            }
+            retVal.games.push({
+              internal  : gp.internal,
+              gamename  : gp.gamename,
+              scheduling: gp.scheduling,
+              log       : gp.log,
+              team      : team
+            });
+            cb();
+          })
+        },
+        function (err) {
+          // That's it iterated through all found teams
+          if (err) {
+            logger.error('Error in async loop', err);
+            return res.status(500).send({message: err.message});
+          }
+          res.send(retVal);
+        });
+    });
+
   });
 });
 
