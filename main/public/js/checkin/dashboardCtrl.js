098e1523cb616ee3ed9a281e0af22777ceed0879
patrickm53 <patrickmcenaney53@gmail.com>
2016-01-15 21:36:06 +0100

    Tiles for dashboard added (colors may change...)

--- main/public/js/checkin/dashboardCtrl.js
+++ main/public/js/checkin/dashboardCtrl.js
@@ -5,11 +5,22 @@
 
 'use strict';
 
+$(document).ready(function () {
+  // Make all elements the same height using jquery
+  var boxes     = $('.dashboard-tile');
+  var maxHeight = Math.max.apply(
+    Math, boxes.map(function () {
+      return $(this).height();
+    }).get());
+  boxes.height(maxHeight);
+});
+
+
 function dashboardCtrl($scope, $http) {
   $scope.chancelleryAsset = 0;
 
 
-  checkinDatastore.dataStore.subscribe('chancellery', function(data) {
+  checkinDatastore.dataStore.subscribe('chancellery', function (data) {
     console.log(data);
     $scope.chancelleryAsset = data.asset;
     $scope.$apply();
