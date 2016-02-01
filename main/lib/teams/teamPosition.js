27bd2df96620a076d45c284762a7b86a11a11ee5
patrickm53 <patrickmcenaney53@gmail.com>
2016-02-01 19:54:00 +0100

    Log only when game active

--- main/lib/teams/teamPosition.js
+++ main/lib/teams/teamPosition.js
@@ -8,17 +8,37 @@
 var ferroSocket;
 var logger         = require('../../../common/lib/logger').getLogger('teams:teamPositions');
 var travelLogModel = require('../../../common/models/travelLogModel');
+var gameCache      = require('../gameCache');
 
 function addLog(data) {
-  travelLogModel.addPositionEntry(data.gameId, data.teamId, {
-    lat     : data.position.lat,
-    lng     : data.position.lng,
-    accuracy: data.position.accuracy
-  }, err => {
+  gameCache.getGameData(data.gameId, (err, gc) => {
     if (err) {
-      logger.error(err);
+      return logger.error(err);
     }
+
+    /**
+     * Privacy: we log the position of the teams only during the game, not when they access
+     * the web page before or afterwards. But there is a certain tolerance given, log also
+     * a little before and after the game (could be helpful if one team is missing on the
+     * way to the game respectively when coming home after the game)
+     */
+    var start = moment(gc.gameplay.scheduling.gameStartTs).subtract(2, 'hours');
+    var end   = moment(gc.gameplay.scheduling.gameEndTs).add(60, 'minutes');
+    if (moment().isAfter(end) || moment().isBefore(start)) {
+      return;
+    }
+
+    travelLogModel.addPositionEntry(data.gameId, data.teamId, {
+      lat     : data.position.lat,
+      lng     : data.position.lng,
+      accuracy: data.position.accuracy
+    }, err => {
+      if (err) {
+        logger.error(err);
+      }
+    });
   });
+
 }
 
 function onPlayerLocation(data) {
