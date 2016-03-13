48fcd76d5503ecb173c3838ae337cb51d7e32635
patrickm53 <patrickmcenaney53@gmail.com>
2016-03-13 12:32:46 +0100

    Show flag for owned properties and stars for each house

--- main/public/js/checkin/pricelistCtrl.js
+++ main/public/js/checkin/pricelistCtrl.js
@@ -6,13 +6,25 @@
 'use strict';
 
 /**
-* The pricelist controller
-* @param $scope
-* @param $http
-*/
+ * The pricelist controller
+ * @param $scope
+ * @param $http
+ */
 function pricelistCtrl($scope) {
   $scope.pricelist = ferropoly.pricelist;
 
+  // properties Updates
+  checkinDatastore.dataStore.subscribe('properties', function (data) {
+    var props = data.properties;
+    for (var i = 0; i < props.length; i++) {
+      var entry = _.find($scope.pricelist, {uuid: props[i].uuid});
+      if (entry) {
+        console.log('Updating Entry', entry);
+        entry.gamedata = props[i].gamedata;
+      }
+    }
+    $scope.$apply();
+  });
 }
 
 checkinApp.controller('pricelistCtrl', pricelistCtrl);
