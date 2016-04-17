9b16a847dd81f4658ce553c5b714ed67354f9def
patrickm53 <patrickmcenaney53@gmail.com>
2016-04-17 17:40:14 +0200

    Better method of showing all markers on the map

--- main/public/js/infoctrl.js
+++ main/public/js/infoctrl.js
@@ -4,7 +4,7 @@
  */
 'use strict';
 var map;
-var mapCenter;
+var mapBounds;
 
 /**
  * Show the correct panel using JQuery
@@ -20,28 +20,23 @@ function showPanel(p) {
 
 
   _.delay(function () {
-
     // Force the update
-    google.maps.event.trigger(map, 'resize');
-    map.setCenter(mapCenter);
+    google.maps.event.trigger(map, 'resize')
+    map.fitBounds(mapBounds);
   }, 250);
 
 }
 
 var infoControl = angular.module('infoApp', []);
 infoControl.controller('infoCtrl', ['$scope', '$http', function ($scope, $http) {
-  var ICON_EDIT_LOCATION = 'https://maps.gstatic.com/mapfiles/ms2/micons/red-dot.png';
-  var ICON_TRAIN_LOCATION = 'https://maps.gstatic.com/mapfiles/ms2/micons/green.png';
-  var ICON_BUS_LOCATION = 'https://maps.gstatic.com/mapfiles/ms2/micons/yellow.png';
-  var ICON_BOAT_LOCATION = 'https://maps.gstatic.com/mapfiles/ms2/micons/blue.png';
+  var ICON_EDIT_LOCATION     = 'https://maps.gstatic.com/mapfiles/ms2/micons/red-dot.png';
+  var ICON_TRAIN_LOCATION    = 'https://maps.gstatic.com/mapfiles/ms2/micons/green.png';
+  var ICON_BUS_LOCATION      = 'https://maps.gstatic.com/mapfiles/ms2/micons/yellow.png';
+  var ICON_BOAT_LOCATION     = 'https://maps.gstatic.com/mapfiles/ms2/micons/blue.png';
   var ICON_CABLECAR_LOCATION = 'https://maps.gstatic.com/mapfiles/ms2/micons/purple.png';
-  var ICON_OTHER_LOCATION = 'https://maps.gstatic.com/mapfiles/ms2/micons/pink.png';
+  var ICON_OTHER_LOCATION    = 'https://maps.gstatic.com/mapfiles/ms2/micons/pink.png';
 
   var currentProperty;
-  var lngMax = 0;
-  var lngMin = 180;
-  var latMax = 0;
-  var latMin = 180;
 
   /**
    * Set the icon for this location in the map
@@ -86,15 +81,15 @@ infoControl.controller('infoCtrl', ['$scope', '$http', function ($scope, $http) 
    * Show the info for a location
    * @param marker
    */
-  var showInfoWindow = function (marker) {
-    var pos = marker.property.pricelist.position + 1;
+  var showInfoWindow     = function (marker) {
+    var pos  = marker.property.pricelist.position + 1;
     var desc = '<h3>' + marker.property.location.name + '</h3>';
     desc += 'Position in Preisliste: ' + pos + '<br/>';
     desc += 'Preis: ' + marker.property.pricelist.price + '<br/>';
     desc += 'Maximale Miete: ' + marker.property.pricelist.rents.hotel + '<br/>';
     desc += 'Erreichbarkeit: ' + marker.property.location.accessibility + '<br/>';
     new google.maps.InfoWindow({
-      content: desc,
+      content : desc,
       maxWidth: 200
     }).open(map, marker);
   };
@@ -108,25 +103,20 @@ infoControl.controller('infoCtrl', ['$scope', '$http', function ($scope, $http) 
     var latSum = 0;
     var lngSum = 0;
 
+    mapBounds = new google.maps.LatLngBounds();
+
     for (var i = 0; i < pl.length; i++) {
       var newMarker = new google.maps.Marker({
-        position: new google.maps.LatLng(pl[i].location.position.lat, pl[i].location.position.lng),
-        map: map,
+        position : new google.maps.LatLng(pl[i].location.position.lat, pl[i].location.position.lng),
+        map      : map,
         draggable: false
       });
 
-      var lat = pl[i].location.position.lat;
-      var lng = pl[i].location.position.lng;
-      latSum += parseFloat(lat);
-      lngSum += parseFloat(lng);
-      if (lat > latMax) latMax = lat;
-      if (lat < latMin) latMin = lat;
-      if (lng > lngMax) lngMax = lng;
-      if (lng < lngMin) lngMin = lng;
-
+      // Make sure all markers are on the map
+      mapBounds.extend(newMarker.getPosition());
 
       newMarker.property = pl[i];
-      pl[i].marker = newMarker;
+      pl[i].marker       = newMarker;
 
       setMarkerIcon(newMarker.property);
       // This is a special procedure allowing all markers to be unique (scope problem)!
@@ -138,8 +128,9 @@ infoControl.controller('infoCtrl', ['$scope', '$http', function ($scope, $http) 
     }
     //setCurrentProperty($scope.markers[0]);
     // $scope.setVisibleMarkers();
-    mapCenter = new google.maps.LatLng(latSum / i, lngSum / i);
-    map.setCenter(mapCenter);
+    //mapCenter = new google.maps.LatLng(latSum / i, lngSum / i);
+    //map.setCenter(mapCenter);
+    map.fitBounds(mapBounds);
   };
 
   /**
@@ -148,16 +139,16 @@ infoControl.controller('infoCtrl', ['$scope', '$http', function ($scope, $http) 
   $(document).ready(function () {
     showPanel('#p1');
 
-    $scope.gp = gp;
-    $scope.pl = pl;
+    $scope.gp    = gp;
+    $scope.pl    = pl;
     $scope.teams = teams;
 
     var mapOptions = {
-      center: new google.maps.LatLng(47.29725, 8.867215),
-      zoom: 8,
+      center   : new google.maps.LatLng(47.29725, 8.867215),
+      zoom     : 8,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    map = new google.maps.Map(document.getElementById("map_canvas"),
+    map            = new google.maps.Map(document.getElementById("map_canvas"),
       mapOptions);
 
     google.maps.event.addListener(map, 'zoom_changed', function (e) {
@@ -168,35 +159,7 @@ infoControl.controller('infoCtrl', ['$scope', '$http', function ($scope, $http) 
         map.setZoom(14);
       }
     });
-    google.maps.event.addListener(map, 'ds', function (e) {
-      console.log(e);
-      var center = map.getCenter();
-      var latNew = center.lat();
-      var lngNew = center.lng();
-      var changed = false;
 
-      if (center.lat() > latMax) {
-        latNew = latMax;
-        changed = true;
-      }
-      if (center.lat() < latMin) {
-        latNew = latMin;
-        changed = true;
-      }
-      if (center.lng() > lngMax) {
-        lngNew = lngMax;
-        changed = true;
-      }
-      if (center.lng() < lngMin) {
-        lngNew = lngMin;
-        changed = true;
-      }
-
-      if (changed) {
-        console.log(latNew + ' + ' + lngNew);
-        map.setCenter(new google.maps.LatLng(latNew, lngNew));
-      }
-    });
     initPropertyMarkers();
     $scope.$apply();
   });
