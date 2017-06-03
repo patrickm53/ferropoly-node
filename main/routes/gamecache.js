721b3d002a927acf25a901aaec0bfb3285383f22
patrickm53 <patrickmcenaney53@gmail.com>
2017-06-03 14:10:54 +0200

    gamechache error codes

--- main/routes/gamecache.js
+++ main/routes/gamecache.js
@@ -4,27 +4,26 @@
  */
 
 
-var express = require('express');
-var router = express.Router();
-var gamecache = require('../lib/gameCache');
-var gameScheduler = require('../lib/gameScheduler');
-var logger = require('../../common/lib/logger').getLogger('routes:gamecache');
+const express = require('express');
+const router = express.Router();
+const gamecache = require('../lib/gameCache');
+const gameScheduler = require('../lib/gameScheduler');
+const logger = require('../../common/lib/logger').getLogger('routes:gamecache');
 
 /**
- * Get the ranking list
+ * Refreshing the game cache
  */
 router.post('/refresh', function (req, res) {
   gamecache.refreshCache(function(err) {
     if (err) {
       logger.error('can not refresh cache', err);
-      res.send({status:'error', message:err.message});
-      return;
+      return res.status(500).send({message: 'refreshCache error: ' + err.message});
     }
     gameScheduler.update(function(err) {
       if (err) {
         logger.error('can not update scheduler', err);
         res.send({status:'error', message:err.message});
-        return;
+        return res.status(500).send({message: 'gameScheduler.update error: ' + err.message});
       }
       logger.info('Cache and scheduler updated');
       res.send({status:'ok'});
