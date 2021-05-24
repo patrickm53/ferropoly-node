b67367526a45ed5dfa9f51590f0c1ad29ebb041d
patrickm53 <patrickmcenaney53@gmail.com>
2021-05-24 16:57:19 +0200

    Confirming players

--- main/lib/summaryMailer.js
+++ main/lib/summaryMailer.js
@@ -20,20 +20,20 @@ function SummaryMailer(scheduler) {
   let self = this;
 
   this.scheduler = scheduler;
-  this.mailer    = mailer.init(settings);
+  mailer.init(settings);
 
   if (this.scheduler) {
     /**
      * This is the 'interest' event launched by the gameScheduler
      */
     this.scheduler.on('summary', function (event) {
-      logger.info(`Summary requested for ${event.gameId}`)
+      logger.info(`Summary requested for ${event.gameId}`);
       self.sendInfo(event.gameId, err => {
         if (err) {
           return logger.error(err);
         }
         event.callback(err, event);
-      })
+      });
     });
   }
 }
@@ -51,7 +51,7 @@ SummaryMailer.prototype.sendInfo = function (gameId, callback) {
       }
 
       // Do not send any information for Demo games!
-      if (gameData.internal.isDemo) {
+      if (gameData.gameplay.internal.isDemo) {
         logger.info(`NOT sending Summary Email, ${gameId} is a demo game!`);
         return callback();
       }
@@ -62,22 +62,22 @@ SummaryMailer.prototype.sendInfo = function (gameId, callback) {
       teams.forEach(t => {
         bccString += _.get(t, 'data.teamLeader.email', '') + ',';
       });
-      bccString += 'info@ferropoly.ch'
+      bccString += 'info@ferropoly.ch';
 
       let html = '';
       let text = '';
 
       html += '<h1>Ferropoly Spielinfo</h1>';
-      html += `<p>Salü!</p>`;
+      html += '<p>Salü!</p>';
       html += '<p>Wir hoffen, Du hattest viel Spass am vergangenen Ferropoly!</p>';
       html += '<p>Die Zusammenfassung des Spiels steht nun für 30 Tage zur Ansicht unter diesem Link bereit: ';
       html += `<a href="${settings.server.url}/summary/${gameId}">${settings.server.url}/summary/${gameId}</a></p>`;
       html += '<p>Viele Grüsse vom Ferropoly-Team!</p>';
 
-      text += 'Salü\nWir hoffen, Du hattest viel Spass am vergangenen Ferropoly!\n'
-      text += 'Die Zusammenfassung des Spiels steht nun für 30 Tage zur Ansicht unter diesem Link bereit:\n'
-      text += `${settings.server.url}/summary/${gameId}\n\n`
-      text += 'Viele Grüsse vom Ferropoly-Team!'
+      text += 'Salü\nWir hoffen, Du hattest viel Spass am vergangenen Ferropoly!\n';
+      text += 'Die Zusammenfassung des Spiels steht nun für 30 Tage zur Ansicht unter diesem Link bereit:\n';
+      text += `${settings.server.url}/summary/${gameId}\n\n`;
+      text += 'Viele Grüsse vom Ferropoly-Team!';
 
       mailer.send({
         to     : 'noreply@ferropoly.ch',
@@ -88,18 +88,18 @@ SummaryMailer.prototype.sendInfo = function (gameId, callback) {
         text   : text
       }, err => {
         if (err) {
-          return callback(err)
+          return callback(err);
         }
         logger.info(`Summary Email for ${gameId} sent`);
         callback();
-      })
+      });
     });
   }
   catch (ex) {
     logger.error(ex);
     callback(ex);
   }
-}
+};
 
 
 module.exports = {
