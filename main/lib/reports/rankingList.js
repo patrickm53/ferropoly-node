3ead1d4426d6d9610ea8a65ad04d881ae0f92d51
patrickm53 <patrickmcenaney53@gmail.com>
2021-10-01 19:56:59 +0200

    Bugfix null reference ranking list

--- main/lib/reports/rankingList.js
+++ main/lib/reports/rankingList.js
@@ -4,9 +4,10 @@
  */
 
 const teamAccount = require('../../lib/accounting/teamAccount');
-const teamModel = require('../../../common/models/teamModel');
-const moment = require('moment-timezone');
-const xlsx = require('node-xlsx');
+const teamModel   = require('../../../common/models/teamModel');
+const moment      = require('moment-timezone');
+const xlsx        = require('node-xlsx');
+const _           = require('lodash');
 
 module.exports = {
   /**
@@ -25,7 +26,9 @@ module.exports = {
         }
         let xlist = [['Rangliste']];
         for (let i = 0; i < ranking.length; i++) {
-          xlist.push([i + 1, teams[ranking[i].teamId].data.name, ranking[i].asset]);
+          xlist.push([i + 1,
+                      _.get(teams[ranking[i].teamId], 'data.name', 'Fehler: Kein Name!'),
+                      _.get(ranking[i], 'asset', 0)]);
         }
         xlist.push(['Stand: ' + moment.tz(moment(), 'Europe/Zurich').format('D.M.YYYY HH:mm')]);
         callback(null, xlist);
@@ -43,7 +46,7 @@ module.exports = {
         return callback(err);
       }
       let xbuffer = xlsx.build([{name: 'Rangliste', data: xlist}]);
-      let prefix = moment.tz(moment(), 'Europe/Zurich').format('YYMMDD-HHmmss');
+      let prefix  = moment.tz(moment(), 'Europe/Zurich').format('YYMMDD-HHmmss');
       callback(null, {
         data: xbuffer,
         name: prefix + '-' + gameId + '-rangliste.xlsx'
