e5dd7d989b3c204b4c2f058ac1c0cd52c78ddd9f
patrickm53 <patrickmcenaney53@gmail.com>
2021-12-11 12:40:50 +0100

    eslint for vue

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021 Christian Kuster, CH-8342 Wernetshausen, christian@kusti.ch
+ */
+
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+     //'eslint:recommended',
+     'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
