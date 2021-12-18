4a12e18e8b18249c4b465e483ec38bb6ccd4e9bf
patrickm53 <patrickmcenaney53@gmail.com>
2021-12-18 12:46:31 +0100

    Game info added

--- main/webapp/reception/components/overview/gameInfo.vue
+++ main/webapp/reception/components/overview/gameInfo.vue
@@ -1,26 +1,67 @@
 <!---
-
+  Info card about the game
   Christian Kuster, CH-8342 Wernetshausen, christian@kusti.ch
   Created: 17.12.21
 -->
 <template lang="pug">
   div
-    h1 gameInfo
-  
+    ferro-card(title="Spielinfo")
+      b-table-simple(small)
+        b-tbody
+          b-tr
+            b-td Spieldatum
+            b-td {{gameDate | formatGameDate}}
+          b-tr
+            b-td Spielstart
+            b-td {{gameStart | formatGameTime}}
+          b-tr
+            b-td Spielende
+            b-td {{gameEnd | formatGameTime}}
+          b-tr
+            b-td Dauer einer Spielrunde
+            b-td {{interestInterval}} min
+          b-tr
+            b-td Startgeld pro Runde
+            b-td {{interest | formatPrice}}
+          b-tr
+            b-td Guthaben zu Spielbeginn
+            b-td {{startCapital | formatPrice}}
+          b-tr
+            b-td Zinsrunden bei Spielende
+            b-td {{interestCyclesAtEndOfGame}}
+          b-tr
+            b-td Strafzins bei negativem Vermögen
+            b-td {{debtInterest}} %
+
 </template>
 
 <script>
+import FerroCard from '../../../common/components/ferro-card/ferro-card.vue';
+import {mapFields} from 'vuex-map-fields';
+import {formatGameDate, formatGameTime, formatPrice} from '../../../common/lib/formatters';
+
 export default {
-  name: "GameInfo",
-  components: {},
-  filters   : {},
+  name      : 'GameInfo',
+  components: {FerroCard},
+  filters   : {formatGameDate, formatGameTime, formatPrice},
   mixins    : [],
   model     : {},
   props     : {},
   data      : function () {
     return {};
   },
-  computed  : {},
+  computed  : {
+    ...mapFields({
+      gameDate                 : 'gameplay.scheduling.gameDate',
+      gameStart                : 'gameplay.scheduling.gameStart',
+      gameEnd                  : 'gameplay.scheduling.gameEnd',
+      interestInterval         : 'gameplay.gameParams.interestInterval',
+      interest                 : 'gameplay.gameParams.interest',
+      startCapital             : 'gameplay.gameParams.startCapital',
+      interestCyclesAtEndOfGame: 'gameplay.gameParams.interestCyclesAtEndOfGame',
+      debtInterest             : 'gameplay.gameParams.debtInterest',
+    })
+  },
   created   : function () {
   },
   methods   : {}
