278d1f3e59c5716d5608fd530b68d534cc39c373
patrickm53 <patrickmcenaney53@gmail.com>
2022-05-01 08:43:56 +0200

    removed unused old files

--- main/public/js/reception/datastore/datastoreStarter.js
+++
@@ -1,9 +0,0 @@
-/**
- * Call as last datastore module: instantiate it
- * Created by kc on 11.12.15.
- */
-
-var dataStore = new DataStore(ferropoly, ferropolySocket); // ferropoly is defined in the main view
-
-// Just update properties after starting up
-dataStore.updateProperties();
--- main/public/wiz-markdown/README.md
+++
@@ -1,6 +0,0 @@
-# Wiz Markdown
-
-Der Autor dieser Library hat sein GitHub Projekt geschlossen, deshalb
-ist der letzte Stand in Git abgelegt.
-
-Version 1.3.0
