38bb0aca66409d7fcd89dc0cd8ec5362d91f017c
patrickm53 <patrickmcenaney53@gmail.com>
2022-07-17 14:39:20 +0200

    Runs with vue2

--- copyCommons.sh
+++ copyCommons.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp -R ../ferropoly_editor/common/** ./common
-cp -R ../ferropoly_editor/editor/webapp/common/** ./main/webapp/common/
+cp -R ../ferropoly-editor/common/** ./common
+cp -R ../ferropoly-editor/editor/webapp/common/** ./main/webapp/common/
